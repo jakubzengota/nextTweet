7d6993307111fa0f87cca62d2f0f66f14a44a355
asztandar <sztandaradam@gmail.com>
2024-02-18 21:58:29 +0100

    added template component

---
+++ nexttweet/src/app/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+
+
+const Header = () =>{
+    return(
+        <section id="header" className="w-full bg-blue-500 p-[20px] flex justify-between ">
+            
+        </section>
+    )
+}
+export default Header;
---
+++ nexttweet/src/app/components/Template/LandingPage.tsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+
+
+const LandingPage = () =>{
+    return(
+        <main>
+            <Header />
+        </main>
+    )
+}
+
+export default LandingPage;
