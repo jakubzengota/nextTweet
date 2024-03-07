0c020521bf76419fe9b9b6508a4365144fa41aee
asztandar <sztandaradam@gmail.com>
2024-03-07 13:06:31 +0100

    created landing page

--- nexttweet/src/app/components/Template/LandingPage.tsx
+++ nexttweet/src/app/components/Template/LandingPage.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
 import Children from "../Elements/Children/Children";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import styles from "./LandingPage.module.css";
+import WelcomeImg from "../../../assets/welcome.png";
 type TLandingPage = {
     children?: React.ReactNode;
 };
@@ -9,13 +11,39 @@ type TLandingPage = {
 const LandingPage = ({ children }: TLandingPage) => {
     return (
         <main className={styles.main}>
-            <Header className="h-[100px] bg-yellow" />
-            <div className="flex-grow bg-blue overflow-auto" style={{height: `calc(100vh - 100px - 150px)`}}>
-                <Children>
-                    {children}
+            <Header className={styles.header} />
+                <Children className={styles.children}>
+                    {
+                        children?
+                        <>{children}</>
+                        :
+                        <>
+                        <section className={styles.children_default}>
+                            <div className={styles.children_default_left}>
+                                <span className={styles.children_default_left_mainText}>
+                                    Stwórzmy wspołną sieć informacji
+                                </span>
+                                <span className={styles.children_default_left_secondText}>
+                                    Dziel się swoimi informacjami z innymi.
+                                </span>
+                                <div className={styles.children_default_left_divButton}>
+                                    <button className={styles.buttonWelcome}>Zarejestruj się</button>
+                                </div>
+                            </div>
+                            <div className={styles.children_default_right}>
+                                <Image src={WelcomeImg} alt="welcome" id={styles.welcome}/>
+                            </div>
+
+                        </section>
+                        <section className={styles.opinion}>
+a
+                        </section>
+                        
+                        </>
+                        
+                    }
                 </Children>
-            </div>
-            <Footer className="h-[150px] bg-red" />
+            <Footer className={styles.footer} />
         </main>
     );
 }
