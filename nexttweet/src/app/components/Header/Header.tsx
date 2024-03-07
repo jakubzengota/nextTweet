69043023f278da115f900674e1d0a0dc2bd7fbf8
asztandar <sztandaradam@gmail.com>
2024-03-07 13:06:44 +0100

    created header module

--- nexttweet/src/app/components/Header/Header.tsx
+++ nexttweet/src/app/components/Header/Header.tsx
@@ -7,14 +7,19 @@ interface IHeaderProps {
 }
 
 const Header: React.FC<IHeaderProps> = ({ className }) => {
+
+  const login = () => {
+    window.location.href = '/login'
+  }
+
   return (
     <header id="header" className={styles.header}>
       <section id="logo_section" className="logo_section">
         <Image src={Logo} alt="logo" id="logo" className={styles.logo} />
       </section>
-      <section id="menu_header" className="menu_header">
+      <section id="menu_header" className={styles.menu_header}>
         <nav className={styles.nav}>
-          <button className={styles.button_menu}>Zaloguj się</button>
+          <button className={styles.button_menu} onClick={()=>login()}>Zaloguj się</button>
           <button className={styles.button_menu}>Zarejestruj się</button>
         </nav>
       </section>
