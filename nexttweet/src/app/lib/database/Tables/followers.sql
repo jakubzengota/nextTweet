829c5e6d7ac6e55dd7689741b405291b5cb7b32c
asztandar <sztandaradam@gmail.com>
2024-03-08 23:35:35 +0100

    refactor

--- nexttweet/src/app/lib/database/Tables/followers.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE followers (
-    follower_id SERIAL PRIMARY KEY,
-    follower_user_id INT REFERENCES users(user_id),
-    followed_user_id INT REFERENCES users(user_id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CHECK (follower_user_id != followed_user_id)
-
-);
--- nexttweet/src/app/lib/database/Tables/media.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE media (
-    media_id SERIAL PRIMARY KEY,
-    tweet_id INT REFERENCES tweets(tweet_id),
-    media_url VARCHAR(255) NOT NULL,
-    media_type VARCHAR(50) NOT NULL CHECK (media_type IN ('image', 'video')),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
