dd4d955116b1b2dd56ececb750e2e0259c8446ee
asztandar <sztandaradam@gmail.com>
2024-04-12 08:58:08 +0200

    refactor

--- nexttweet/src/app/page.tsx
+++ nexttweet/src/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
-import Head from 'next/head';
-import Link from 'next/link';
-import styles from '../../styles/Home.module.css';
-import LandingPage from './components/Template/LandingPage';
+import LandingPage from '../templates/LandingPage/LandingPage'
 
 export default function Home() {
   return (
