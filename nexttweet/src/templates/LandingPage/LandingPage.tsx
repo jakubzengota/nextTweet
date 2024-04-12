7f3da0a818a3892767ec6484a1f16a2ab61cc9e6
asztandar <sztandaradam@gmail.com>
2024-04-12 08:59:56 +0200

    added lp template

--- nexttweet/src/templates/LandingPage/LandingPage.tsx
+++ nexttweet/src/templates/LandingPage/LandingPage.tsx
@@ -1,8 +1,21 @@
-
-const LandingPageTemplate = () => {
+import Header from "../../components/Header/Header";
+import Child from "../../components/Child/Child";
+import Footer from "../../components/Footer/Footer";
+import LandingPageWelcome from "@/components/LandingPageWelcome/LandingPageWelcome";
+import LandingPageOpinion from "@/components/LandingPageOpinion/LandingPageOpinion";
+const LandingPage = () => {
     return(
-        <main>
-            
+        <main className="main_landingPage">
+            <Header />
+                <Child className="child1">
+                    <LandingPageWelcome />
+                </Child>
+                <Child>
+                    <LandingPageOpinion />
+                </Child>
+            <Footer />
         </main>
     )
 }
+
+export default LandingPage;
