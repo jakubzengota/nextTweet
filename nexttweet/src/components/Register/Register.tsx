a16f3809f3155fb86243a123b02a709be6d75f97
asztandar <sztandaradam@gmail.com>
2024-04-12 09:00:30 +0200

    added register component

---
+++ nexttweet/src/components/Register/Register.tsx
@@ -0,0 +1,104 @@
+import Link from "next/link";
+import { useState } from "react";
+
+const Register = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+
+        if (password !== confirmPassword) {
+            alert('Passwords do not match');
+            return;
+        }
+
+        const response = await fetch('/api/Auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password, email }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert('Rejestracja udana: ' + data.message);
+        } else {
+            alert('Błąd rejestracji: ' + data.message);
+        }
+    };
+
+    const handleInputChange = (event: any) => {
+        const { id, value } = event.target;
+        if (id === 'username') setUsername(value);
+        if (id === 'password') setPassword(value);
+        if (id === 'confirmPassword') setConfirmPassword(value);
+        if (id === 'email') setEmail(value);
+    };
+
+
+    const handleSignUpWithGoogle = () => {
+        // handle sign up with Google action
+    };
+
+
+    return (
+        <section className="register">
+            <form className="form" onSubmit={handleSubmit}>
+                <h3>REGISTER HERE</h3>
+                <label htmlFor="username">Username</label>
+                <input
+                    className='inputImportant'
+                    type="text"
+                    placeholder="Username"
+                    id="username"
+                    value={username}
+                    onChange={handleInputChange}
+                />
+
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    id="email"
+                    value={email}
+                    onChange={handleInputChange}
+                />
+
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    placeholder="Password"
+                    id="password"
+                    value={password}
+                    onChange={handleInputChange}
+                />
+
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    id="confirmPassword"
+                    value={confirmPassword}
+                    onChange={handleInputChange}
+                />
+
+                <div>
+                    Already have an account? <Link
+                        style={{ color: "black" }}
+                        href="/login"
+                    >
+                        <span>Sign in</span>
+                    </Link>
+                </div>
+
+                <button className='button' type="submit">Register</button>
+            </form>
+        </section>
+    )
+}
+export default Register;
