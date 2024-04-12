f8a03259bf219c3023511849b8a0ac4fd858e5fd
asztandar <sztandaradam@gmail.com>
2024-04-12 08:56:22 +0200

    created header section

--- nexttweet/src/components/Header/Header.tsx
+++ nexttweet/src/components/Header/Header.tsx
@@ -2,28 +2,36 @@ import Image from "next/image"
 import { useTranslation } from "react-i18next";
 import LogoImg from "../../assets/logo.png"
 import Link from "next/link";
+import React from "react";
+import { IInterfaceContext, IModalsContext, InterfaceContext, ModalsContext } from "@/Context/ModalsContext";
+
 
 const Header = () => {
+
+
+
     const { t : translate } = useTranslation();
+    const { languageModal } = React.useContext(ModalsContext) as IModalsContext;    
     return(
-        <header className="header_landingpage">
+        <header className="header">
             <section className="header_section_logo">
-                <Image src={LogoImg} alt={translate("LogoAlt")} />
+                <Image src={LogoImg} priority alt={translate("LogoAlt")} className="header_section_logo_img"/>
             </section>
             <section>
                 <div className="header_section_nav">
-                    <nav>
-                        <Link href="/login">{translate("LoginLink")}</Link>
-                        <Link href="/register">{translate("RegisterLink")}</Link>
+                    <nav className="header_section_nav_links">
+                        <Link href="/login" className="header_section_nav_link">{translate("LoginLink")}</Link>
+                        <Link href="/register" className="header_section_nav_link">{translate("RegisterLink")}</Link>
                     </nav>
                     <div className="header_section_nav_lang">
-                        <Image src={} alt={} />
+                        <button className="header_section_nav_lang_btn" onClick={languageModal.openModalLanguage}>
+                            PL
+                        </button>
                     </div>
-                </div>
-                <div className="header_section_nav">
-
                 </div>
             </section>
         </header>
     )
 }
+
+export default Header;
